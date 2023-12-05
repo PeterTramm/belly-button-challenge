dc4949abe660fda91e3c8cb407b78933bfa4f8f0
peter.extra <peter.extra@outlook.com>
2023-12-06 09:53:54 +1100

    Recoded loaded data and got defult bar graph

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,53 +1,73 @@
-//defining url link
+// Define url 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-// Reading samples.json using D3
+// Set up dimensions of the chart
+const margin = {top:70, right:40,bottom:60,left:175}
+const width = 600 - margin.left - margin.right
+const height = 400 - margin.top - margin.bottom
 
-d3.json(url).then(function(data) {
-    console.log(data);
-    console.log(data.samples);
+//Set up SVG container 
+
+const svg = d3.select(".container").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height",height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "Translate("+margin.left + "," + margin.top + ")");
+
+// Load and process data 
+d3.json(url).then(data => {
+    sampleValues = [];
+    otuIds = [];
+    otuLabels = [];
+
+    data.samples.forEach(d => {
+        sampleValues.push(d.sample_values);
+        otuLabels.push(d.otu_labels);
+        otuIds.push(d.otu_ids)
+    });
     
-    // get values for the bar chart
-    let sampleValues = []
-    for (let i = 0; i < 10; i ++) {
-        sampleValues.push(data.samples[i]['sample_values'])
-    }
-    console.log(sampleValues);
+    console.log(otuLabels); // List of bacteria Names
+    console.log(otuIds); // List of bacteria identifciation numbers
+    console.log(sampleValues); // List of counts of bacteria Ids
 
-    let otuIds = []
-    for (let i = 0; i < 10; i ++) {
-        otuIds.push(data.samples[i]['otu_ids'])
-}
-    console.log(otuIds);
-
-    let otuLabels = []
-    for (let i = 0; i < 10; i ++) {
-        otuLabels.push(data.samples[i]['otu_labels'])
-}
-    console.log(otuLabels);
-
-    let data1 = {
-        "x": sampleValues,
-        "y": otuIds
-    }
-    // Plotting graph
-    //defining function 
-    function init() {
-        let data = [{
-          values: data1,
-          labels: otuIds,
-          type: "bar"
-        }];
-      
-        let layout = {
-          height: 600,
-          width: 800
-        };
-      
-        Plotly.newPlot("bar", data, layout);
-      };
+    // console.log(data.samples)
+    // console.log(sampleValues)
     
-      init();
+
+
+    //Set up x and y scales
+    const x = d3.scaleLinear()
+        .range([0,width])
+        .domain([0, d3.max(sampleValues, function (sampleValue) {return sampleValue})]);
+    
+    const y = d3.scaleBand()
+        .range([height,0])
+        .padding(0.1)
+        .domain(sampleValues.map(function(sampleValues) {return sampleValues}))
+
+    // Create the x and y axes
+    const xAxis = d3.axisBottom(x)
+
+    const yAxis = d3.axisLeft(y)
+
+    // Add the x and y Axes to the chart 
+    svg.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+
+    svg.append("g")
+    .call(yAxis);
+
+    // Create the Bar fors the chart
+    svg.selectAll(".bar")
+    .data(sampleValues)
+    .enter().append("rect")
+    .attr("class","bar")
+    .attr("y",function (sampleValues) {return y(sampleValues)})
+    .attr("height", y.bandwidth())
+    .attr("x", 0)
+    .attr("width", function (sampleValues) {return x(sampleValues);})
+    .style("fill","skyblue")
 });
-
 
